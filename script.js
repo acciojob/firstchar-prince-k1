a797992a2fa276e2c142edc99165a147ff686b57
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-11-08 17:55:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstChar(text) {
   // your code here
+	let arr = text.split('');
+	let res ='';
+	for(let i in arr){
+		if(arr[i] != ' '){
+			res = arr[i];
+			break;
+		}
+	}
+	return res;
 }
 
 // Do not change the code below
